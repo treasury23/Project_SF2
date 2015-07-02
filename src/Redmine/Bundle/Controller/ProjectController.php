067c5b5e9f5a371bd30262f5b262c95a5c050f58
Viktor Musienko <treasury23@gmail.com>
2015-07-02 14:51:50 +0300

    add list project

---
+++ src/Redmine/Bundle/Controller/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Redmine\Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Redmine;
+
+class ProjectController extends Controller
+{
+    public function showAction(Request $request)
+    {
+        $project_id = $request->query->get('id');
+        $client = new Redmine\Client('https://redmine.ekreative.com', '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c');
+
+        $issues=$client->api('issue')->showIssueProject($project_id);
+
+
+        return $this->render('RedmineBundle:Project:showProject.html.twig',array('project'=>$project_id,'issues'=>$issues));
+    }
+}
--- src/Redmine/Bundle/Controller/UserController.php
+++ src/Redmine/Bundle/Controller/UserController.php
@@ -5,28 +5,34 @@ namespace Redmine\Bundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
+use Redmine;
 
 class UserController extends Controller
 {
     public function homeAction()
     {
-        return $this->render('RedmineBundle:User:index.html.twig');
+        $client = new Redmine\Client('https://redmine.ekreative.com', '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c');
+
+        $projects=$client->api('project')->all([
+            'limit' => 1000
+        ]);
+
+        return $this->render('RedmineBundle:User:index.html.twig',array('client'=>$client,'projects'=>$projects{'projects'}));
     }
 
     public function loginAction(Request $request)
     {
+
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render(
             'RedmineBundle:User:login.html.twig',
             array(
-                // last username entered by the user
+
                 'last_username' => $lastUsername,
                 'error'         => $error,
             )
